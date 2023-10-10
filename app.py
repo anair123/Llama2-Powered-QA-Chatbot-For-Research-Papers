a696b6b4005323d90bdd0cc9805dc7cb5b310c8a
anair123 <aashishnair8925@gmail.com>
2023-10-10 11:41:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pickle
 
 
 st.set_page_config(page_title='Llama2-Chatbot')
-st.header('Custom Llama2-Powered Chatbot')
+st.header('Custom Llama2-Powered Chatbot :robot_face:')
 
 @st.cache_resource()
 def load_llm():
@@ -76,29 +76,39 @@ def generate_response(query, qa_chain):
 
 
 def get_user_input():
+
+    # get the user query
     input_text = st.text_input('Ask me anything about the use of computer vision in sports!', "", key='input')
     return input_text
 
+
+
+# create the qa_chain
 qa_chain = create_qa_chain()
 
+
+# create empty lists for user queries and responses
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
-
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+# get the user query
 user_input = get_user_input()
 
 
 if user_input:
 
+    # generate response to the user input
     response = generate_response(query=user_input, qa_chain=qa_chain)
 
+    # add the input and response to session state
     st.session_state.past.append(user_input)
     st.session_state.generated.append(response)
 
-if st.session_state['generated']:
 
+# display conversaion history (if there is one)
+if st.session_state['generated']:
     for i in range(len(st.session_state['generated']) -1, -1, -1):
         message(st.session_state['generated'][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
